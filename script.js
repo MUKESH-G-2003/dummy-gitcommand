8836b31e4d9b408eb334d3ea02a30658968318e7
MUKESH G <127624466+MUKESH-G-2003@users.noreply.github.com>
2024-09-02 10:36:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let fetchRes = fetch(
+    "https://jsonplaceholder.typicode.com/todos/1");
+        
+    fetchRes.then(res =>
+        res.json()).then(d => {
+            console.log(d)
+        })
+    
